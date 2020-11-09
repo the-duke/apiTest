f3aad7ab95957d0f4ba03e58bad2363ed1cde3f0
the-duke <woody.woodpusher@gmail.com>
2020-11-09 12:44:30 +0100

    changes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const filmsRequest = fetch('https://ghibliapi.herokuapp.com/films'),
-      peopleRequest = fetch('https://ghibliapi.herokuapp.com/people');
-const app = document.getElementById('app');
-const movies = document.createElement('div');
+const filmsUrl = 'https://ghibliapi.herokuapp.com/films',
+      peopleUrl = 'https://ghibliapi.herokuapp.com/people';
+
+const app = document.getElementById('app'),
+      movies = document.createElement('div');
+      
 movies.setAttribute('class', 'movies');
 app.appendChild(movies);
